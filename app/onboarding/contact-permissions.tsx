48757448ebf729dbe0ce50cabb780d959013845e
Adampatryk <adamkulpa16@gmail.com>
2025-07-12 19:11:47 +0100

    Contacts permission

--- app/onboarding/contact-permissions.tsx
+++ app/onboarding/contact-permissions.tsx
@@ -3,6 +3,11 @@ import { Button, Text, View } from 'react-native';
 
 export default function ContactPermissions() {
   const router = useRouter();
+
+  const handleAllowAccess = () => {
+    // Request contact permissions
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Sync your contacts</Text>
